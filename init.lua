157d680ba6089ac73dd14349b06248585405f3dc
John <john.sjostrom@valtech.com>
2024-05-28 16:55:39 +0200

    adding plugins

--- init.lua
+++ init.lua
@@ -726,7 +726,8 @@ require('lazy').setup({
         -- python = { "isort", "black" },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
-        -- javascript = { "prettierd", "prettier", stop_after_first = true },
+        javascript =  { 'prettierd', 'prettier', stop_after_first = true  },
+        typescriptreact =  { 'prettierd', 'prettier',  stop_after_first = true },
       },
     },
   },
@@ -951,12 +952,12 @@ require('lazy').setup({
   --    This is the easiest way to modularize your config.
   --
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
-  -- { import = 'custom.plugins' },
   --
   -- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
   -- Or use telescope!
   -- In normal mode type `<space>sh` then write `lazy.nvim-plugin`
   -- you can continue same window with `<space>sr` which resumes last telescope search
+  { import = 'custom.plugins' },
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
