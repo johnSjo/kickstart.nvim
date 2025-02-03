de1bba1fafe199442d30831edeea644208ef6412
John <john@debian.home>
2025-02-03 07:36:42 +0100

    adding move line key for linux

--- lua/john0x28/remap.lua
+++ lua/john0x28/remap.lua
@@ -1,14 +1,23 @@
 vim.g.mapleader = ' '
+vim.opt.linebreak = true
+
 vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
 vim.keymap.set('n', '<C-d>', '<C-d>zz', { silent = true })
 vim.keymap.set('n', '<C-u>', '<C-u>zz', { silent = true })
 vim.keymap.set('n', '<C-y>', 'kzz', { silent = true })
 vim.keymap.set('n', '<C-e>', 'jzz', { silent = true })
+
+-- moving lines
+vim.keymap.set('n', '<C-j>', ':m+1<CR>==', { silent = true })
+vim.keymap.set('n', '<C-k>', ':m-2<CR>==', { silent = true })
+vim.keymap.set('v', '<C-j>', ":m'>+1<CR>`<my`>gv=gv", { silent = true })
+vim.keymap.set('v', '<C-k>', ':m-2<CR>==gv', { silent = true })
+-- moving lines for mac
 vim.keymap.set('n', '∆', ':m+1<CR>==', { silent = true })
 vim.keymap.set('n', '˚', ':m-2<CR>==', { silent = true })
--- vim.keymap.set('v', '∆', ':m+1<CR>==gv', { silent = true })
 vim.keymap.set('v', '∆', ":m'>+1<CR>`<my`>gv=gv", { silent = true })
 vim.keymap.set('v', '˚', ':m-2<CR>==gv', { silent = true })
+
 vim.keymap.set('n', '<C-s>', '<cmd>write<cr>', {})
 vim.keymap.set('i', '<C-s>', '<cmd>write<cr>', {})
 vim.keymap.set('n', '<leader>f', '<cmd>Neotree reveal<cr>', {})
